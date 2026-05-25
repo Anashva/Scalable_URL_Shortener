173fd589ad246bbb0c381d2c78d138457252e9dd
Anashva <anashva2406@gmail.com>
2026-05-25 12:27:18 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+
+
+
+app.use(express.json());
+
+app.get("/" ,(req,res)=>{
+    res.send("server running");
+});
+
+app.listen(8000,()=>{
+    console.log("server is running on port 8000");
+})
